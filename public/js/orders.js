2b3bfaeaab8544e27daf6b74351413981f759ffa
roohollah68 <rshirkhany@gmail.com>
2023-12-04 08:14:11 +0330

    fix

--- public/js/orders.js
+++ public/js/orders.js
@@ -261,37 +261,37 @@ function generatePDFs() {
         $.notify('ابتدا باید سفارشات مورد نظر را انتخاب کنید', 'error')
         return
     }
-    let data = {
-        _token: token,
-        ids: ids
-    };
-    fetch('pdfs/', {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json",
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
-    });
-    // return response.json(); // parses JSON response into native JavaScript objects
-
-
-    return;
-
-    $.post('pdfs/', {
-        _token: token,
-        ids: ids
-    })
-        .done(res => {
-
-        })
-
-    return;
+    // let data = {
+    //     _token: token,
+    //     ids: ids
+    // };
+    // fetch('pdfs/', {
+    //     method: "POST", // *GET, POST, PUT, DELETE, etc.
+    //     mode: "cors", // no-cors, *cors, same-origin
+    //     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    //     credentials: "same-origin", // include, *same-origin, omit
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         // 'Content-Type': 'application/x-www-form-urlencoded',
+    //     },
+    //     redirect: "follow", // manual, *follow, error
+    //     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    //     body: JSON.stringify(data), // body data type must match "Content-Type" header
+    // });
+    // // return response.json(); // parses JSON response into native JavaScript objects
+    //
+    //
+    // return;
+    //
+    // $.post('pdfs/', {
+    //     _token: token,
+    //     ids: ids
+    // })
+    //     .done(res => {
+    //
+    //     })
+    //
+    // return;
 
 
     let dialog = [];
