ce9b17de83d34b3f0394f31ff7643e140c1a8e9c
roohollah68 <rshirkhany@gmail.com>
2024-01-30 00:04:02 +0330

    Update style_css.blade.php

--- resources/views/layout/style_css.blade.php
+++ resources/views/layout/style_css.blade.php
@@ -15,44 +15,48 @@
         font-weight: bold;
     }
 
-    body{
+    * {
+        font-family: IranSans;
+    }
+
+    body {
         text-align: right;
         margin-bottom: 150px !important;
     }
 
     .ui-widget {
-        font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
         font-size: 1em;
     }
 
     .form-group.required label:after {
-        content:"*";
-        color:red;
+        content: "*";
+        color: red;
     }
 
-    .printed ,.printed span , .printed b{
+    .printed, .printed span, .printed b {
         font-size: 80px;
         direction: rtl;
         font-family: BNazanin;
     }
 
-    .printed span{
+    .printed span {
         font-size: 90px;
     }
 
-    .long-text ,.long-text span,.long-text b{
+    .long-text, .long-text span, .long-text b {
         font-size: 73px;
     }
 
-    .short-text ,.short-text span,.short-text b{
+    .short-text, .short-text span, .short-text b {
         font-size: 99px;
     }
 
-    canvas{
+    canvas {
         direction: rtl;
     }
 
-    .dataTables_wrapper{
+    .dataTables_wrapper {
         overflow-x: scroll;
     }
 </style>
