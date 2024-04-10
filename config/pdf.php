3a62ce58bd60e26b4885fc9cd5b2814a73a59166
roohollah68 <rshirkhany@gmail.com>
2024-04-10 12:59:11 +0330

    Update pdf.php

--- config/pdf.php
+++ config/pdf.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    'mode' => '',
     'format' => [200, 100],
     'default_font' => 'iransans',
     'margin_left' => 2,
@@ -9,8 +10,24 @@ return [
     'margin_header' => 0,
     'margin_footer' => 0,
     'orientation' => 'P',
+    'title' => 'Laravel mPDF',
+    'subject' => '',
+    'author' => '',
+    'watermark' => '',
+    'show_watermark' => false,
+    'show_watermark_image' => false,
+    'watermark_font' => 'sans-serif',
     'display_mode' => 'fullpage',
+    'watermark_text_alpha' => 0.1,
+    'watermark_image_path' => '',
+    'watermark_image_alpha' => 0.2,
+    'watermark_image_size' => 'D',
+    'watermark_image_position' => 'P',
     'auto_language_detection' => false,
+    'temp_dir' => storage_path('app'),
+    'pdfa' => false,
+    'pdfaauto' => false,
+    'use_active_forms' => false,
     'custom_font_dir' => base_path('public/css/'), // don't forget the trailing slash!
     'custom_font_data' => [
         'iransans' => [ // must be lowercase and snake_case
@@ -31,7 +48,7 @@ return [
         ],
     ]
 ];
-
+/*
 return [
     'mode' => '',
     'format' => 'A4',
