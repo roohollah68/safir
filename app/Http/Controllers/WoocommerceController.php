499d46f8c37ca8bdd63ce2fcc28377f18af730bd
roohollah68 <rshirkhany@gmail.com>
2024-08-31 08:32:57 +0330

    Update WoocommerceController.php

--- app/Http/Controllers/WoocommerceController.php
+++ app/Http/Controllers/WoocommerceController.php
@@ -16,7 +16,7 @@ class WoocommerceController extends Controller
         //$this->sendMessageToBale(["text" =>file_get_contents('php://input')],'1444566712');
         $request = json_decode(file_get_contents('php://input'));
         if (env('APP_ENV') == 'local')
-            $request = json_decode(file_get_contents('woo/1403-4-22_05-48-03 _ peptina _ کوثر امیرخانی.txt'));
+            $request = json_decode(file_get_contents('woo/1403-6-10_08-10-04 _ peptina _ ابراهیم قطبی نژاد.txt'));
         if (!isset($request->billing))
             return 'not used';
         file_put_contents('woo/' . verta(null, "Asia/Tehran")->
