9949b074747f5882d472bfcb4f3d9a4b9af88b98
roohollah68 <rshirkhany@gmail.com>
2024-10-15 05:06:27 +0330

    Update WoocommerceController.php

--- app/Http/Controllers/WoocommerceController.php
+++ app/Http/Controllers/WoocommerceController.php
@@ -16,7 +16,7 @@ class WoocommerceController extends Controller
         //$this->sendMessageToBale(["text" =>file_get_contents('php://input')],'1444566712');
         $request = json_decode(file_get_contents('php://input'));
         if (env('APP_ENV') == 'local')
-            $request = json_decode(file_get_contents('woo/1403-6-10_20-21-53 _ peptina _ زهرا درودی.txt'));
+            $request = json_decode(file_get_contents('woo/1403-7-24_04-57-04 _ peptina _ تینا شمسایی.txt'));
         if (!isset($request->billing))
             return 'not used';
         file_put_contents('woo/' . verta(null, "Asia/Tehran")->
@@ -116,7 +116,7 @@ class WoocommerceController extends Controller
                 }
             } else {
                 $order = $web->order()->first();
-                $order = $order->update($orderData);
+                $order->update($orderData);
                 if (!$order->deleted_at) {
                     app('Telegram')->deleteOrderFromBale($order, env('GroupId'));
                     $order->delete();
