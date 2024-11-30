6a62277f9c345974f133bed48f231dcb742b5026
roohollah68 <rshirkhany@gmail.com>
2024-11-30 18:21:18 +0330

    Update SettingController.php

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -80,6 +80,12 @@ class SettingController extends Controller
 
     public function command()
     {
+        $customers = Customer::with('user')->get();
+        foreach ($customers as $customer){
+            if(!$customer->user){
+                $customer->forceDelete();
+            }
+        }
         set_time_limit(0);
 
     }
