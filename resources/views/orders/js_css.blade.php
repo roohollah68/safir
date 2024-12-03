448c8e3f85a75fd8187dcc3400a627b3a8e004de
roohollah68 <rshirkhany@gmail.com>
2024-12-03 19:06:58 +0330

    Update js_css.blade.php

--- resources/views/orders/js_css.blade.php
+++ resources/views/orders/js_css.blade.php
@@ -118,7 +118,7 @@
             table.rows.add(data);
             table.draw();
         } else {
-            let hideCols = (print || superAdmin) ? [1, 8, 9, 10, 11, 12, 13, 14] : [0, 1, 3, 8, 9, 10, 11, 12, 13, 14]
+            let hideCols = (changeOrdersPermit) ? [1, 8, 9, 10, 11, 12, 13, 14] : [0, 1, 3, 8, 9, 10, 11, 12, 13, 14]
             table = $('#main-table').DataTable({
                 columns: [
                     {title: "انتخاب"},
