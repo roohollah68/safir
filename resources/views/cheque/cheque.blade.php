761b57224a998a010217d0a79c01c475d2d0f605
sinshr <98868456+sinnshr@users.noreply.github.com>
2025-02-06 19:37:18 +0330

    fix cheque filters

--- resources/views/cheque/cheque.blade.php
+++ resources/views/cheque/cheque.blade.php
@@ -33,23 +33,15 @@
         $(function() {
             const date1 = new mds.MdsPersianDateTimePicker($('#from_date')[0], {
                 targetTextSelector: '#from_date',
-                textFormat: 'yyyy/MM/dd',
-                isGregorian: false,
-                enableTimePicker: false,
-                englishNumber: true,
                 @if (isset($from))
-                    selectedDate: '{{ $from }}',
+                    selectedDate: new Date('{{ $from }}'),
                 @endif
             });
 
             const date2 = new mds.MdsPersianDateTimePicker($('#to_date')[0], {
                 targetTextSelector: '#to_date',
-                textFormat: 'yyyy/MM/dd',
-                isGregorian: false,
-                enableTimePicker: false,
-                englishNumber: true,
                 @if (isset($to))
-                    selectedDate: '{{ $to }}',
+                    selectedDate: new Date('{{ $to }}'),
                 @endif
             });
         });
@@ -67,11 +59,6 @@
             background: none !important;
         }
 
-        .state-filter-btn.active {
-            background-color: #007bff;
-            color: white;
-        }
-
         .filter-section {
             display: flex;
             align-items: center;
@@ -87,6 +74,20 @@
         .filter-section input {
             width: 100px;
         }
+
+        .btn-radio input[type="radio"] {
+            display: none;
+        }
+
+        .btn-radio input[type="radio"]:checked+label {
+            background-color: #007bff;
+            color: white;
+        }
+
+        .btn-primary {
+            margin-right: 30px;
+            width: 80px;
+        }
     </style>
 @endsection
 
@@ -96,136 +97,154 @@
 
         {{-- فیلترها --}}
 
-        <form action="{{ route('cheque.filterChequeDate') }}" method="get">
-            <div class="filter-section mb-4">
-                <i>از تاریخ: </i><input type="text" value="{{ $from ?? '' }}" style="width: 120px" id="from_date"
-                    name="from">
-                <i class="mx-3"></i>
-                <i>تا تاریخ: </i><input type="text" value="{{ $to ?? '' }}" style="width: 120px" id="to_date"
-                    name="to">
-            </div>
+        <div class="mb-4">
+            <form action="#" method="post">
+                @csrf
+                <div class="filter-section mb-3">
+                    <label for="from">از تاریخ: </label>
+                    <input type="text" class="form-control" style="width: 120px" id="from_date" name="from">
+                    <label for="to">تا تاریخ: </label>
+                    <input type="text" class="form-control" style="width: 120px" id="to_date" name="to">
+                </div>
 
-            <select name="state">
-                <option value="">همه</option>
-                <option value="1">پاس شده</option>
-                <option value="0">پاس نشده</option>
-            </select>
-            <button type="submit">فیلتر</button>
-        </form>
+                <div class="filter-section">
+                    <label for="state">وضعیت:</label>
 
-        {{-- <form action="{{ route('cheque.setNextMonth') }}" method="get">
-            @csrf
-            <button type="submit">چک‌های 30 روز آینده</button>
-        </form>
+                    <input type="radio" class="btn-check" id="all" name="state" value=""
+                        {{ request('state') === '' ? 'checked' : '' }}>
+                    <label class="btn btn-outline-primary" for="all">همه</label>
 
-        <form action="{{ route('cheque.pastCheques') }}" method="get">
-            @csrf
-            <button type="submit">چک‌های تاریخ گذشته</button>
-        </form>  --}}
+                    <input type="radio" class="btn-check" id="passed" name="state" value="1"
+                        {{ request('state') === '1' ? 'checked' : '' }}>
+                    <label class="btn btn-outline-success" for="passed">پاس شده</label>
 
-        {{-- جداول --}}
+                    <input type="radio" class="btn-check" id="not_passed" name="state" value="0"
+                        {{ request('state') === '0' ? 'checked' : '' }}>
+                    <label class="btn btn-outline-warning" for="not_passed">پاس نشده</label>
 
-        <div id="tabs" dir="rtl">
-            <ul class="nav nav-tabs" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active" id="received-tab" data-bs-toggle="tab" href="#received" role="tab"
-                        aria-controls="received" aria-selected="true">
-                        چک‌های دریافتی
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="given-tab" data-bs-toggle="tab" href="#given" role="tab"
-                        aria-controls="given" aria-selected="false">
-                        چک‌های پرداختی
-                    </a>
-                </li>
-            </ul>
+                    <div class="vr ms-3 mb-2"></div>
 
-            <div id="received" class="tab-pane fade show active">
-                <h3 class="mt-4 mb-4">چک‌های دریافتی</h3>
+                    <div class="filter-section ms-1">
 
-                <table id="receivedTable" class="table table-striped mt-4 mb-4" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th>نام صاحب چک</th>
-                            <th>تاریخ چک</th>
-                            <th>مبلغ (ریال)</th>
-                            <th>کد چک</th>
-                            <th>وضعیت</th>
-                            <th>عملیات</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($receivedCheque as $cheque)
-                            <tr>
-                                <td>
-                                    <a href="/customer/transaction/{{ $cheque->customer_id }}"
-                                        class="text-primary text-decoration-none hover:text-decoration-underline">
-                                        {{ $cheque->cheque_name }}
-                                    </a>
-                                </td>
-                                <td>{{ verta($cheque->cheque_date)->formatJalaliDate() }}</td>
-                                <td>{{ number_format($cheque->amount) }}</td>
-                                <td>{{ $cheque->cheque_code }}</td>
-                                <td>
-                                    @if ($cheque->cheque_pass)
-                                        <input type="button" class="btn btn-success" value="پاس شده" disabled>
-                                    @else
-                                        <input type="button" class="btn btn-warning" value="پاس نشده"
-                                            onclick="confirmPassCheque({{ $cheque->id }}, 'received')">
-                                    @endif
-                                </td>
-                                <td>
-                                    <div class="ms-2">
-                                        <span class="fa fa-eye btn btn-info"
-                                            onclick="view_recieved_cheque({{ $cheque->id }})" title="مشاهده"></span>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                        <input type="radio" class="btn-check" id="next" name="next" value="1">
+                        <label class="btn btn-outline-secondary" for="next">چک‌های آتی</label>
 
-            <div id="given" class="tab-pane fade">
-                <h3 class="mt-4 mb-4">چک‌های پرداختی</h3>
-                <table id="givenTable" class="table table-striped mt-4 mb-4" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th>نام حساب</th>
-                            <th>تاریخ چک</th>
-                            <th>مبلغ (ریال)</th>
-                            <th>شناسه ملی</th>
-                            <th>وضعیت</th>
-                            <th>عملیات</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($givenCheque as $cheque)
-                            <tr>
-                                <td>{{ $cheque->account_name }}</td>
-                                <td>{{ verta($cheque->cheque_date)->formatJalaliDate() }}</td>
-                                <td>{{ number_format($cheque->amount) }}</td>
-                                <td>{{ $cheque->cheque_id }}</td>
-                                <td>
-                                    @if ($cheque->cheque_pass)
-                                        <input type="button" class="btn btn-success" value="پاس شده" disabled>
-                                    @else
-                                        <input type="button" class="btn btn-warning" value="پاس نشده"
-                                            onclick="confirmPassCheque({{ $cheque->id }}, 'given')">
-                                    @endif
-                                </td>
-                                <td>
-                                    <div class="ms-2">
-                                        <span class="fa fa-eye btn btn-info"
-                                            onclick="view_given_cheque({{ $cheque->id }})" title="مشاهده"></span>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                        <input type="radio" class="btn-check" id="previous" name="previous" value="0">
+                        <label class="btn btn-outline-danger" for="previous">چک‌های گذشته</label>
+
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mb-2">فیلتر</button>
+                </div>
         </div>
+        </form>
+    </div>
+
+    {{-- جداول --}}
+
+    <div id="tabs" dir="rtl">
+        <ul class="nav nav-tabs" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="received-tab" data-bs-toggle="tab" href="#received" role="tab"
+                    aria-controls="received" aria-selected="true">
+                    چک‌های دریافتی
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="given-tab" data-bs-toggle="tab" href="#given" role="tab" aria-controls="given"
+                    aria-selected="false">
+                    چک‌های پرداختی
+                </a>
+            </li>
+        </ul>
+
+        <div id="received" class="tab-pane fade show active">
+            <h3 class="mt-4 mb-4">چک‌های دریافتی</h3>
+
+            <table id="receivedTable" class="table table-striped mt-4 mb-4" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>نام صاحب چک</th>
+                        <th>تاریخ چک</th>
+                        <th>مبلغ (ریال)</th>
+                        <th>کد چک</th>
+                        <th>وضعیت</th>
+                        <th>عملیات</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($receivedCheque as $cheque)
+                        <tr>
+                            <td>
+                                <a href="/customer/transaction/{{ $cheque->customer_id }}"
+                                    class="text-primary text-decoration-none hover:text-decoration-underline">
+                                    {{ $cheque->cheque_name }}
+                                </a>
+                            </td>
+                            <td>{{ isset($cheque->cheque_date) ? verta($cheque->cheque_date)->formatJalaliDate() : 'نامشخص' }}
+                            </td>
+                            <td>{{ number_format($cheque->amount) }}</td>
+                            <td>{{ $cheque->cheque_code }}</td>
+                            <td>
+                                @if ($cheque->cheque_pass)
+                                    <input type="button" class="btn btn-success" value="پاس شده" disabled>
+                                @else
+                                    <input type="button" class="btn btn-warning" value="پاس نشده"
+                                        onclick="confirmPassCheque({{ $cheque->id }}, 'received')">
+                                @endif
+                            </td>
+                            <td>
+                                <div class="ms-2">
+                                    <span class="fa fa-eye btn btn-info"
+                                        onclick="view_recieved_cheque({{ $cheque->id }})" title="مشاهده"></span>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div id="given" class="tab-pane fade">
+            <h3 class="mt-4 mb-4">چک‌های پرداختی</h3>
+            <table id="givenTable" class="table table-striped mt-4 mb-4" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>نام حساب</th>
+                        <th>تاریخ چک</th>
+                        <th>مبلغ (ریال)</th>
+                        <th>شناسه ملی</th>
+                        <th>وضعیت</th>
+                        <th>عملیات</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($givenCheque as $cheque)
+                        <tr>
+                            <td>{{ $cheque->account_name }}</td>
+                            <td>{{ isset($cheque->cheque_date) ? verta($cheque->cheque_date)->formatJalaliDate() : 'نامشخص' }}
+                            </td>
+                            <td>{{ number_format($cheque->amount) }}</td>
+                            <td>{{ $cheque->cheque_id }}</td>
+                            <td>
+                                @if ($cheque->cheque_pass)
+                                    <input type="button" class="btn btn-success" value="پاس شده" disabled>
+                                @else
+                                    <input type="button" class="btn btn-warning" value="پاس نشده"
+                                        onclick="confirmPassCheque({{ $cheque->id }}, 'given')">
+                                @endif
+                            </td>
+                            <td>
+                                <div class="ms-2">
+                                    <span class="fa fa-eye btn btn-info" onclick="view_given_cheque({{ $cheque->id }})"
+                                        title="مشاهده"></span>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -217,15 +217,12 @@ Route::middleware(['auth', 'verify'])->group(function () {
     });
 
     //CHEQUES
-    Route::controller(ChequeController::class)->prefix('cheque')->group(function () {
+   Route::controller(ChequeController::class)->prefix('cheque')->group(function () {
         Route::get('/cheque', 'cheque')->name('chequeList');
+        Route::post('/cheque', 'cheque')->name('chequeList');
         Route::get('/given/{id}', 'view')->name('cheque.view');
         Route::get('/recieved/{id}', 'recievedView')->name('cheque.view2');
         Route::post('/pass', 'passCheque');
-        Route::get('/cheque/filterChequeDate', 'filterChequeDate')->name('cheque.filterChequeDate');
-        Route::get('/cheque/filterPassedCheques', 'filterPassedCheques')->name('cheque.filterPassedCheques');
-        Route::get('/cheque/setNextMonth','setNextMonth')->name('cheque.setNextMonth');
-        Route::get('/cheque/past', 'pastCheques')->name('cheque.pastCheques');
     });
 
 
@@ -236,9 +233,8 @@ Route::middleware(['auth', 'verify'])->group(function () {
         Route::get('/edit/{id}', 'edit');
         Route::get('/list', 'list');
     });
+
     ///PROGRAMMER
-
-
     Route::get('/woocommerce/{website}', [WoocommerceController::class, 'addWebsiteOrder']);
     Route::get('/woocommerce', [WoocommerceController::class, 'viewFile']);
 
