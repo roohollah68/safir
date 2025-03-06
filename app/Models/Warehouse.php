7298fe1f5db16d1d8d3eec8f77a069cd268c6864
roohollah68 <rshirkhany@gmail.com>
2025-03-06 14:41:25 +0330

    Update Warehouse.php

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Warehouse extends Model
 {
     use HasFactory;
-    use SoftDeletes;
+//    use SoftDeletes;
     protected $fillable = [
         'phone',
     ];
